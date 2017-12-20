894531618ba1a07ff8670cc65489e11720d5b454
Sara Bartell <saracbartell@gmail.com>
2017-12-19 18:20:58 -0800

    Set up rectangle variables with loop to parse through the entire image dataset and write over them

--- box_images.rb
+++ box_images.rb
@@ -24,10 +24,31 @@ module ElephantParser
     CSV.foreach(filename, headers: true, header_converters: :symbol) do |row|
         @images << Elephant.new(row)
     end
-    p "****"
+
     @images.each do |object|
       if !object.shapes["shapes"].empty?
-        p object.shapes["shapes"][0]
+        image_link = object.image_url
+        p "****"
+        object.shapes["shapes"].each_with_index do |hash, i|
+
+          image = Magick::Image.read(image_link).first
+          bb = image.bounding_box
+          bb.x = object.shapes["shapes"][i]["x"]
+          bb.y = object.shapes["shapes"][i]["y"]
+          bb.width = object.shapes["shapes"][i]["width"]
+          bb.height = object.shapes["shapes"][i]["height"]
+
+          gc = Magick::Draw.new
+          gc.stroke('green')
+          gc.fill_opacity(0)
+
+          gc.rectangle(bb.x, bb.y, bb.x+bb.width, bb.y+bb.height)
+
+          gc.draw(image)
+
+          image.write('new-#{i}.gif')
+          exit
+        end
       end
     end
   end
