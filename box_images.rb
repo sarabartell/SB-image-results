08a22eb1f5fbaf36c0f6e39f37a97d62cdb849ef
Sara Bartell <saracbartell@gmail.com>
2017-12-19 16:08:58 -0800

    Set parse method for data to run through image manipulation

--- box_images.rb
+++ box_images.rb
@@ -1,8 +1,37 @@
 require 'rmagick'
 require 'csv'
+require 'json'
 
 class Elephant
+  attr_reader :image_url
+  attr_accessor :shapes
 
+  def initialize(args={})
+    @shapes = JSON.parse(args[:annotation])
+    @image_url = args[:image_url]
+  end
 
+  def shapes=(value)
+    @shapes = value
+  end
 
 end
+
+module ElephantParser
+  def self.parse(filename)
+    @images = []
+
+    CSV.foreach(filename, headers: true, header_converters: :symbol) do |row|
+        @images << Elephant.new(row)
+    end
+    p "****"
+    @images.each do |object|
+      if !object.shapes["shapes"].empty?
+        p object.shapes["shapes"][0]
+      end
+    end
+  end
+end
+
+elephants = ElephantParser.parse('data.csv')
+
